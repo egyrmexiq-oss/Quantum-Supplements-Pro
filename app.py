dafbbfbfbc9e76ab39f6601be31ac8af0ff346c5
egyrmexiq-oss <egyrmexiq@gmail.com>
2026-01-25 19:21:46 +0000

    se movio el codigo musica mas abajo

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import google.generativeai as genai
 import json
 import os
 from rules import SEGURIDAD_SUPLEMENTOS
-    # Música
-st.audio("https://cdn.pixabay.com/audio/2022/05/27/audio_1808fbf07a.mp3", loop=True, autoplay=True)
+
 
 # ==========================================
 # 1. CONFIGURACIÓN Y ESTILO
@@ -38,6 +37,9 @@ def inyectar_estilo_quantum():
         """, unsafe_allow_html=True)
 
 inyectar_estilo_quantum()
+
+    # Música
+st.audio("https://cdn.pixabay.com/audio/2022/05/27/audio_1808fbf07a.mp3", loop=True, autoplay=True)
 
 # ==========================================
 # 2. SISTEMA DE ESTADÍSTICAS
